60489513ff4823b83b67d7474047056738854d34
ahmtemel <ahmetsalihtml@gmail.com>
2026-03-31 02:04:58 +0300

    p1 finished libvirt

--- p1/scripts/server_setup.sh
+++ p1/scripts/server_setup.sh
@@ -6,19 +6,19 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update && apt-get install -y curl
 
 # 2. Install K3s Server
-# --write-kubeconfig-mode 644 allows you to use kubectl without sudo
-# --node-ip specifies which network interface K3s should use
+# Not: libvirt altında eth1 arayüzü K3s trafiği için kullanılacak
 curl -sfL https://get.k3s.io | sh -s - server \
     --node-ip=192.168.56.110 \
-        --bind-address=192.168.56.110 \
-        --flannel-iface=eth1 \
+    --bind-address=192.168.56.110 \
+    --advertise-address=192.168.56.110 \
+    --flannel-iface=eth1 \
     --write-kubeconfig-mode=644
 
-# 3. Share the token via the /vagrant folder (synced by Vagrant)
-# The worker needs this token to join the cluster
+# 3. Token paylaşımı
+# K3s'in token üretmesini bekle
 until [ -s /var/lib/rancher/k3s/server/node-token ]; do
-    sleep 1
+    sleep 2
 done
 
-cp /var/lib/rancher/k3s/server/node-token /vagrant/.node-token.tmp
-mv /vagrant/.node-token.tmp /vagrant/node-token
+# Vagrant klasörü üzerinden token'ı worker'a ilet
+cp /var/lib/rancher/k3s/server/node-token /vagrant/node-token
